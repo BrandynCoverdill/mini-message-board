127e1b66730306dd5eb930762e3ad8c9a52e6eb6
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-06-26 08:24:43 -0600

    updated process.env variables in code

--- config/database.js
+++ config/database.js
@@ -5,13 +5,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const sequelize = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASS,
+	process.env.MYSQLDATABASE,
+	process.env.MYSQLUSER,
+	process.env.MYSQLPASSWORD,
 	{
-		host: process.env.DB_HOST,
+		host: process.env.MYSQLHOST,
 		dialect: 'mysql',
-		port: process.env.DB_PORT || 3306,
+		port: process.env.MYSQLPORT || 3306,
 		logging: false,
 		define: {
 			timestamps: false,
